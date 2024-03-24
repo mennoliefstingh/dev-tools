bde7a04cbf83028ce39398bb823d3d828ac785d5
Menno Liefstingh <git@menno.ai>
2024-03-24 01:03:42 +0100

    style: Improve aliases in .gitignore and aliases.sh

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -2,7 +2,6 @@
 alias alli='cd ~/alliander'
 alias dev='cd ~/dev/python'
 alias dl='cd ~/Downloads'
-alias ..='cd ..'
 
 # Git aliases
 alias gs='git status'
@@ -13,16 +12,20 @@ alias fix='isort . && black .'
 
 # Python aliases
 alias py='python3'
-alias pi='pip3 install'
-alias v='source venv'
+alias v='venv && source .venv/bin/activate'
+alias d='deactivate'
 
-# Visual Studio Code aliases
-alias c='code'
+# Docker aliases
+alias dc='docker-compose'
+alias dcu='docker-compose up'
+alias db='docker build . -t'
+alias dps='docker ps'
 
 # Shell config
 alias zshconfig="code ~/.zshrc"
-alias reload='chmod +x ~/cli-setup/ && source ~/.zshrc'
 alias cliconfig="code ~/cli-setup"
+alias reload='chmod +x ~/cli-setup/ && source ~/.zshrc'
 
-# server stuff
+# SSH aliases
+alias pi='ssh menno@192.168.1.147'
 alias anton='ssh menno@65.21.182.47'
